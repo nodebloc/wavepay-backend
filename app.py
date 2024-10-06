05371ad980b3e6e9a4280f683285a3e3eefc4fdc
dimka90 <dimkayilrit@gmail.com>
2024-10-06 01:02:44 +0100

    Backend: Added a function that retrive the VTpass categories services

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 # Access environment variables
 VT_API_URL = os.getenv('VT_API_URL')
-VT_SANDBOX_URL = os.getenv('VT_SANDBOX_URL')
+
 API_KEY = os.getenv('API_KEY')
 PUBLIC_KEY = os.getenv('PUBLIC_KEY')
 
@@ -23,30 +23,39 @@ def hello_world():
         }
     }
 
-@app.route('/api/vtpass-balance', methods=['GET'])
-def get_vtpass_balance():
-    headers = {
-        'api-key': API_KEY,
-        'public-key': PUBLIC_KEY
-    }
+# function for connect to Vt Pass Telecom
 
+def connectToApi(vtPassEndpoint, errorMessage,exceptionErrorMessage):
+    headers = {'api-key': API_KEY,'public-key': PUBLIC_KEY}
     try:
         # Make GET request to the VTpass API
-        response = requests.get(VT_SANDBOX_URL, headers=headers)
+        response = requests.get(vtPassEndpoint, headers=headers)
         
         # Check if request was successful
         if response.status_code == 200:
-            balance_data = response.json()  
-            return jsonify(balance_data) 
+            data = response.json()  
+            return jsonify(data) 
         else:
             return jsonify({
-                "error": "Failed to retrieve balance",
+                "error": errorMessage,
                 "status_code": response.status_code,
                 "message": response.text
             }), response.status_code
 
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"status": False, "message": exceptionErrorMessage, "error": str(e)}), 500
+
+@app.route('/api/vtpass-balance', methods=['GET'])
+def get_vtpass_balance():
+    VT_SANDBOX_URL = os.getenv('VT_SANDBOX_URL_BALANCE')
+    result = connectToApi(VT_SANDBOX_URL, "Failed to retrived Balance", "An error Occur")
+    return result
+
+@app.route('/api/vtpass-categories', methods=['GET'])
+def get_vtpass_categories():
+    VT_SANDBOX_URL = os.getenv('VT_SANDBOX_URL_CATEGORIES')
+    result = connectToApi(VT_SANDBOX_URL, "Failed to retrived Categories", "An error Occur")
+    return result
 
 if __name__ == "__main__":
     app.run(debug=True)
